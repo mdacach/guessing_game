1eece9d20d66a0932fa21c1c8f9f766362899a21
matheus <dacachmatheus@gmail.com>
2022-05-05 19:32:44 -0300

    Refactor guess into its own scope
    I am not sure I like the shadowing in same scope pattern of Rust.

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,25 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let guess = {
+            let mut input = String::new();
 
-        let size_in_bytes = std::io::stdin()
-            .read_line(&mut guess) // Will *add* the input to `guess`, not overwrite
-            .expect("Failed to read line"); // Panics if there's an error. Similar to `unwrap()`, but with an error message
+            std::io::stdin()
+                .read_line(&mut input) // Will *add* the input to `guess`, not overwrite
+                .expect("Failed to read line"); // Panics if there's an error. Similar to `unwrap()`, but with an error message
 
-        // Shadowing guess
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(e) => continue,
+            // Convert to u32
+            let guess: u32 = {
+                let parsed = input.trim().parse();
+                match parsed {
+                    Ok(num) => num,
+                    Err(_) => continue,
+                }
+            };
+            guess
         };
-        // From here on, guess is this new value. Not sure if I like this pattern
 
         println!("You guessed: {}", guess);
-        println!("Size (bytes) of input: {}", size_in_bytes);
 
         use std::cmp::Ordering;
         // Note that Rust has now inferred that secret_number is meant to be u32 too!
